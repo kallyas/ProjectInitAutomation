9486105f5336a18d12aa46d21df14a1de44e80ad
Kally <kallyasmedia@gmail.com>
2020-07-26 09:48:10 +0000

    Add Support for windows OS

--- create.py
+++ create.py
@@ -1,18 +1,22 @@
 import sys
 import os
 from github import Github
+from dotenv import load_dotenv
 
-path = "/Users/kally/Documents/Projects/MyProjects/" #Change path to your project default path
+load_dotenv()
 
-username = "" #Insert your github username here
-password = "" #Insert your github password here
+path = os.getenv("FILEPATH")
+username = os.getenv("USERNAME")
+password = os.getenv("PASSWORD")
+
 
 def create():
     folderName = str(sys.argv[1])
-    os.makedirs(path + str(folderName)
+    os.makedirs(path + str(folderName))
     user = Github(username, password).get_user()
     repo = user.create_repo(folderName)
     print("Succesfully created repository {}".format(folderName))
+
 
 if __name__ == "__main__":
     create()
